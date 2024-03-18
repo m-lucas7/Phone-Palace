cdba1eb0b7d707c087f2a94c172852038282d18f
Lucas <lucasmoniez@gmail.com>
2024-03-18 11:52:20 +0100

    update

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-var cors = require('cors')
+const express = require("express");
+var cors = require("cors");
 
 const app = express();
 
@@ -7,29 +7,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/post", require("./routes/post.routes"));
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use(cors())
+app.use(cors());
 
-app.post('/login', (req, res) => {
-  const { mail, password } = req.body;
-  const sql = `SELECT * FROM admin WHERE mail = ? AND password = ?`;
-  connection.query(sql, [mail, password], (err, results) => {
-    if (err) {
-      console.error('Erreur lors de la vérification des identifiants : ', err);
-      res.status(500).json({ message: 'Erreur serveur' });
-      return;
-    }
-    if (results.length > 0) {
-      res.status(200).json({ message: 'Connexion réussie' });
-    } else {
-      res.status(401).json({ message: 'Identifiants invalides' });
-    }
-  });
-});
-
-app.get('/', (req, res) => {
-  res.status(200).send('OK');
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
 });
 
 app.listen(3000, () => {
