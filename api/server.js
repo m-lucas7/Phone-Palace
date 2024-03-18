0a45b7e67a90d431eacf5db11f0c52626fc3032e
Lucas <lucasmoniez@gmail.com>
2024-03-18 11:42:27 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,17 +1,31 @@
 const express = require('express');
-var cors = require('cors');
 
 const app = express();
 
-// Midleware qui permet de traiter les données POST
 app.use(express.json());
-app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/post", require("./routes/post.routes"));
 app.use(express.static('public'))
 
-// Lancer le serveur
+app.post('/login', (req, res) => {
+  const { mail, password } = req.body;
+  const sql = `SELECT * FROM admin WHERE mail = ? AND password = ?`;
+  connection.query(sql, [mail, password], (err, results) => {
+    if (err) {
+      console.error('Erreur lors de la vérification des identifiants : ', err);
+      res.status(500).json({ message: 'Erreur serveur' });
+      return;
+    }
+    if (results.length > 0) {
+      res.status(200).json({ message: 'Connexion réussie' });
+    } else {
+      res.status(401).json({ message: 'Identifiants invalides' });
+    }
+  });
+});
+
+
 app.listen(3000, () => {
   console.log(`Le serveur est démarré sur le port : 3000`);
 });
