2c2fb7721c2654040709c5c2086546019a55a2b8
baptouu59 <baptistem1720@gmail.com>
2024-03-18 11:22:08 +0100

    AFFICHER PRODUIT
    azaz

--- site/components/article.jsx
+++ site/components/article.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-function Article() {
+const Article = () => {
+  const [printsData, setPrintsData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/post');
+        setPrintsData(response.data);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleAddToCart = (print) => {
+    addItem(print);
+  };
+
   return (
-    <div>
-      <h2>Home</h2>
-      <p>Welcome to our article</p>
+    <div className="centered-container">
+      {printsData ? (
+        <div className="centered-content">
+          <h1>Articles</h1>
+          {printsData.map((print, index) => (
+            <div key={index}>
+              <p>Titre: {print.name}</p>
+              <p>Description: {print.descr}</p>
+              <a href={`detail/${print.id}`} target="_blank">
+                <img src={print.image} alt="" />
+              </a>
+              <p>Prix: {print.price} €</p>
+              <button onClick={() => handleAddToCart(print)}>Ajouter au panier</button>
+              <a href={`detail/${print.id}`} target="_blank">
+                <button>Voir détail</button>
+              </a>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Chargement des données...</p>
+      )}
     </div>
   );
-}
+};
 
 export default Article;
+
